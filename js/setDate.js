478bdb7f5d46e7a38043e612234de30f22f26e4e
Dmitriy Grigoryev <dima.grig2017@mail.ru>
2023-09-21 21:50:59 +0300

    Последняя корректировка

--- js/setDate.js
+++ js/setDate.js
@@ -1,63 +1,34 @@
-let days = Array.from(document.getElementsByClassName("page-nav__day")); // устанавливаем дату в список выше
+let daysOfWeek = ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"];
+let days = Array.from(document.getElementsByClassName("page-nav__day"));
 let date = new Date();
 date.setDate(date.getDate() - 1);
 
 days.forEach((element) => {
-  // дата на следующие дни
   date.setDate(date.getDate() + 1);
   element.children[1].textContent = date.getDate();
-  switch (Number(date.getDay())) {
-    case 1: {
-      element.children[0].textContent = "Пн";
-      break;
-    }
-    case 2: {
-      element.children[0].textContent = "Вт";
-      break;
-    }
-    case 3: {
-      element.children[0].textContent = "Ср";
-      break;
-    }
-    case 4: {
-      element.children[0].textContent = "Чт";
-      break;
-    }
-    case 5: {
-      element.children[0].textContent = "Пт";
-      break;
-    }
-    case 6: {
-      element.children[0].textContent = "Сб";
-      element.classList.add("page-nav__day_weekend");
-      break;
-    }
-    case 0: {
-      element.children[0].textContent = "Вс";
-      element.classList.add("page-nav__day_weekend");
-      break;
-    }
-    default:
-      break;
+  let dayIndex = date.getDay();
+  element.children[0].textContent = daysOfWeek[dayIndex];
+  if (dayIndex === 0 || dayIndex === 6) {
+    element.classList.add("page-nav__day_weekend");
   }
 });
 
-let currentDate = new Date(); // текущая дата
-let selectDay = new Date(); // день который выбрали
+let currentDate = new Date();
+let selectDay = new Date();
 let navDay = document.getElementsByClassName("page-nav")[0];
 navDay
   .getElementsByClassName("page-nav__day_chosen")[0]
-  .classList.toggle("page-nav__day_chosen"); // выключаем выбранный день по умолчанию
-navDay.children[0].classList.toggle("page-nav__day_chosen"); // сегодняшний день делаем активным
+  .classList.toggle("page-nav__day_chosen");
+navDay.children[0].classList.toggle("page-nav__day_chosen");
 
-let pageNavDays = Array.from(document.getElementsByClassName("page-nav__day")); // смена текущей даты
+let pageNavDays = Array.from(document.getElementsByClassName("page-nav__day"));
 pageNavDays.forEach((e, index) => {
   e.onclick = function () {
     document
       .getElementsByClassName("page-nav__day_chosen")[0]
-      .classList.toggle("page-nav__day_chosen"); // выключаем текущий
-    e.classList.toggle("page-nav__day_chosen"); // ставим выбранный
+      .classList.toggle("page-nav__day_chosen");
+    e.classList.toggle("page-nav__day_chosen");
     selectDay.setDate(currentDate.getDate() + index);
-    setFilms(JSON.parse(localStorage.getItem("info")), selectDay); // функция из udateFilm.js обновляем всю информацию под выбранную дату
+    setFilms(JSON.parse(localStorage.getItem("info")), selectDay);
   };
 });
